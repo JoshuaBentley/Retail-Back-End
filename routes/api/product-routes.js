4d4e61caf8f0e4ea34d75b0ac04ab011fbf65ec1
JoshuaBentley <joshuabentley911@gmail.com>
2023-02-20 09:07:52 -0700

    set up routing for products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,14 +1,20 @@
 const router = require('express').Router();
-const { Product, Category, Tag, ProductTag } = require('../../models');
+const { Products, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  try{
+    const allProducts = await Products.findAll({
+     include: [{model: Tag}, {model: Category}] 
+    })
+    res.status(200).json(allProducts)
+   } catch (err) {
+    res.status(500).json(err)
+   }
 });
-
+ 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
